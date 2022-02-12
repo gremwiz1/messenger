a96996d8dd55e20e29305202043a780c6676dc6f
gremwiz1 <gremwiz@yandex.ru>
2022-02-12 18:15:20 +0300

    added login page

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 4000;
+const { PORT = 4000 } = process.env;
 
-app.use(express.static('./'));
+app.use(express.static('dist'));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
