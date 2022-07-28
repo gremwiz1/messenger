bd75fd635ac799e627cea7c436a00dcb646b691e
gremwiz1 <gremwiz@yandex.ru>
2022-07-28 23:44:30 +0300

    added event-bus

---
+++ src/utils/event-bus.ts
@@ -0,0 +1,35 @@
+type Tcallback = (...args: any) => void;
+class EventBus {
+  listeners: { [key: string]: Tcallback[] };
+  constructor() {
+      this.listeners = {};
+  }
+
+  on(event: string, callback: Tcallback):void {
+      if (!this.listeners[event]) {
+          this.listeners[event] = [];
+      }
+
+      this.listeners[event].push(callback);
+}
+
+  off(event: string, callback: Tcallback):void {
+      if (!this.listeners[event]) {
+    throw new Error(`Нет события: ${event}`);
+  }
+
+  this.listeners[event] = this.listeners[event].filter(
+    listener => listener !== callback
+  );
+}
+
+  emit(event: string, ...args: unknown[]):void {
+      if (!this.listeners[event]) {
+              throw new Event(`Нет события: ${event}`);
+      }
+
+      this.listeners[event].forEach(listener => {
+          listener(...args);
+      });
+  }
+}
