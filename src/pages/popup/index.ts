2ec84fa106427ce9ee652e5a07ea3fc7cb1058ea
gremwiz1 <gremwiz@yandex.ru>
2022-08-28 19:21:07 +0300

    update popup

--- src/pages/popup/index.ts
+++ src/pages/popup/index.ts
@@ -1,10 +1,26 @@
-import ButtonLink from "../../component/button-link";
+import Button from "../../component/button";
+import userController from "../../controllers/user-controller";
+import Router from "../../utils/router";
+import store from "../../utils/store";
 
-let Button = new ButtonLink({
+const router = new Router();
+let ButtonSubmit = new Button({
   title: "Поменять",
-  className: "form__button",
+  events: {
+    click: function (e: Event) {
+      e.preventDefault();
+      const formData = new FormData(e.target as HTMLFormElement);
+      const res = userController.changeUserAvatar({ avatar: formData });
+      if (res.status === 200) {
+        store.set("user", res.data);
+        router.go("/settings");
+      } else {
+        console.log("Не удалось изменить аватар");
+      }
+    },
+  },
 });
 
 export const popupProps = {
-  Button: Button,
+  Button: ButtonSubmit,
 };
